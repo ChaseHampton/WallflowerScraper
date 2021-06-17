b59bc8e1c1a83984ab0a5bbd001d876a6b0eeb43
Chase Hampton <chase.hampton10@gmail.com>
2021-06-17 15:41:54 -0500

    Add base scraper logic and json output

---
+++ wallflower.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+headers = {'Connection':'keep-alive','Upgrade-Insecure-Requests':'1','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.48','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+source = requests.get('https://www.bathandbodyworks.com/c/home-fragrance/wallflowers-refills', headers=headers)
+
+soup = BeautifulSoup(source.text, 'lxml')
+div = soup.find(id='search-result-items')
+wfs = []
+morePages = True
+# print(div.text)
+
+
+def CreateJson():
+    with open('wallflowers.json','w') as writer:
+        json.dump(wfs, writer)
+
+
+def NextPage(soup):
+    next = soup.find('a', attrs={"class": 'page-next'})
+    if next:
+        return next['href']
+    else:
+        return False
+
+def ParseWFs(list):
+    for item in list:
+        wfName = item.find(class_="product-name").text.replace('\n\n','')
+        wfPrice = item.find('div', attrs={'class': 'product-pricing'}).span.text
+        wfType = item.find(class_='product-type').text.replace('\n\n','')
+        wfs.append({'name': wfName,'price':wfPrice,'type':wfType})
+
+ParseWFs(div.find_all('li', attrs={'class': re.compile('^grid-tile.*')}))
+while NextPage(soup):
+    source = requests.get(NextPage(soup), headers=headers)
+    soup = BeautifulSoup(source.text, 'lxml')
+    div = soup.find(id='search-result-items')
+    ParseWFs(div.find_all('li', attrs={'class': re.compile('^grid-tile.*')}))
+CreateJson()
