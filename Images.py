c26793d926999346c2ecec67104376cd81c619be
Chase Hampton <chase.hampton10@gmail.com>
2021-06-18 09:14:35 -0500

    Needs main function and cleaning

--- Images.py
+++ Images.py
@@ -1,6 +1,12 @@
 import requests
+import re
 
 def ImageParse(name, img):
+    imgName = re.search(r"\d{9}.*\...g", img)
+
+    print(imgName.group(0))
+
     r = requests.get(img).content
+    
     with open(f"Images\{name}.jpg", "bw+") as imgWriter:
         imgWriter.write(r)
--- wallflower.py
+++ wallflower.py
@@ -3,15 +3,19 @@ import requests
 import re
 import json
 import Images
+import os
+
 
 headers = {'Connection':'keep-alive','Upgrade-Insecure-Requests':'1','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.48','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 source = requests.get('https://www.bathandbodyworks.com/c/home-fragrance/wallflowers-refills', headers=headers)
-
 soup = BeautifulSoup(source.text, 'lxml')
 div = soup.find(id='search-result-items')
 wfs = []     
 imgList = []
 
+if not os.path.isdir('Images'):
+    os.mkdir('Images')
+    
 def CreateJson(data):
     with open('wallflowers.json','w') as writer:
         json.dump(data, writer)
@@ -34,8 +38,8 @@ def ParseWFs(list):
         imgList.append((item.find('img')['src'],wfName))
         
 
-ParseWFs(div.find_all('li', attrs={'class': re.compile('^grid-tile.*')}))
 
+ParseWFs(div.find_all('li', attrs={'class': re.compile('^grid-tile.*')}))
 while NextPage(soup):
     source = requests.get(NextPage(soup), headers=headers)
     soup = BeautifulSoup(source.text, 'lxml')
